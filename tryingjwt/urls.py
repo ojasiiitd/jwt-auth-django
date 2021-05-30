609e47c91a3f5ae7d2de6fddeb445b8d35212eab
Ojas Saxena <ojasva18352@iiitd.ac.in>
2021-05-30 20:08:36 +0530

    jwt

--- tryingjwt/urls.py
+++ tryingjwt/urls.py
@@ -1,6 +1,15 @@
+from django.db.models import fields
 from django.urls import path
+from django.urls.conf import include
 from . import views
+from rest_framework import routers
+from rest_framework_simplejwt import views as jwt_views
+
+router = routers.DefaultRouter()
 
 urlpatterns = [
-    path('home/', views.index, name='index'),
+    path('' , views.index , name="home"),
+    path('api/token/' , jwt_views.TokenObtainPairView.as_view() , name='token_obtain_pair'),
+    path('api/token/refresh/' , jwt_views.TokenRefreshView.as_view() , name='token_refresh'),
+    path('hello/' , views.HelloView.as_view() , name="hello")
 ]
--- tryingjwt/views.py
+++ tryingjwt/views.py
@@ -1,5 +1,15 @@
-from django.http import HttpResponse
+from django.contrib.auth.models import User, Group
+from django.http.response import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 
+def index(response):
+    return HttpResponse('<h1>NEW PAGE</h1>')
 
-def index(request):
-    return HttpResponse("Hello, world. You're at the index.")
+class HelloView(APIView):
+    permission_classes = (IsAuthenticated,)
+
+    def get(self, request):
+        content = {'message': 'Hello, World!'}
+        return Response(content)
